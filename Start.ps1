5676c0f623c928e47f700aeec14a4be96e4991b3
OlaYZen <Olaisenvb@gmail.com>
2023-01-17 10:55:54 +0100

    Earliest Test

---
+++ Start.ps1
@@ -0,0 +1,10 @@
+mkdir "C:\OlaiOS"
+# $source = ''
+# $destination = 'C:\OlaiOS\'
+# Invoke-WebRequest -Uri $source -OutFile $destination
+
+Stop-Process installer.ps1
+Stop-Process
+Stop-Process
+
+mkdir
